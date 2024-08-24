da147dc03ff7ef6efe79b3177c8419240e7f5862
takuya kinoshita <kktak02@gmail.com>
2024-08-24 15:36:19 +0900

    add

---
+++ app/models/user.py
@@ -0,0 +1,32 @@
+from sqlalchemy import String, Integer, Column, Boolean, DateTime
+from datetime import datetime
+
+from models.base import Base, session_scope
+
+
+class UserList(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    password = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+
+    @classmethod
+    def create(cls, id, name, age, password, created_at):
+        user = cls(id=id, name=name, age=age, password=password, created_at=created_at)
+        try:
+            with session_scope() as session:
+                session.add(user)
+        except Exception as e:
+            print(f"error: {e}")
+
+    @classmethod
+    def get(cls, id):
+        with session_scope() as session:
+            user = session.query(cls).filter(cls.id == id).first()
+        if user is None:
+            return None
+        return user
